10096565c3a37571a0a5856217ece3a39eaa3004
ANDRnewHuman <blsckcollapse@gmail.com>
2024-04-27 17:13:04 +0200

    up

--- Temp/test.py
+++ Temp/test.py
@@ -1,4 +1,4 @@
-
+'''
 from pathlib import Path
 
 
@@ -7,21 +7,6 @@ file_name = Path('./Temp')
 def total_salary(path):
     total = 0
     count = 0
-    # for argument in path.iterdir():
-    #     if argument.is_dir():
-    #         print(f"Parse folder: This is folder - {argument.name}")
-    #     if argument.is_file():
-    #         print(f"Parse folder: This is file - {argument.name}")
-
-
-    # try:
-    #     with open(file_name / 'text.txt', 'r', encoding='utf-8') as file:
-    #         for line in file:
-    #             print(line, end='')
-
-    # except Exception as e:
-    #     print(f'{e} with file')
-
     file_path = path.resolve() / 'text.txt'
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -42,4 +27,34 @@ total_salary(file_name)
 
 total, average = total_salary(file_name)
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+'''
 
+
+from pathlib import Path
+file_name = Path('./Temp')
+def get_cats_info(path):
+    cats_info = []
+    file_path = Path(path).resolve() / 'text_cat.txt'
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                cat_data = line.strip().split(',')
+                if len(cat_data) != 3:
+                    continue
+                
+                cat_dict = {
+                    "id": cat_data[0],
+                    "name": cat_data[1],
+                    "age": cat_data[2]
+                }
+                cats_info.append(cat_dict)
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+    except Exception as e:
+        print("Сталася помилка при читанні файлу:", e)
+    
+    return cats_info
+
+cats_info = get_cats_info(file_name)
+print(cats_info)
